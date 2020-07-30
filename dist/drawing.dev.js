190e59d0a431a6aa67e7f0c5ce6778ed62c18408
Joseph Hornby <joehornby@gmail.com>
2020-07-30 17:17:10 +0100

    Add undo/redo history to fabric.js

--- dist/drawing.dev.js
+++ dist/drawing.dev.js
@@ -1,29 +1,49 @@
 "use strict";
 
-window.addEventListener('load', function () {
-  var canvas = new fabric.Canvas('canvas');
-  window.addEventListener('resize', resizeCanvas, false);
-  canvas.backgroundColor = 'transparent';
-  canvas.isDrawingMode = true;
-  canvas.freeDrawingBrush.color = 'red';
-  canvas.freeDrawingBrush.width = 3;
+var canvas = new fabric.Canvas(document.querySelector('canvas'));
+canvas.backgroundColor = 'transparent';
+canvas.isDrawingMode = true;
+canvas.freeDrawingBrush.color = 'red';
+canvas.freeDrawingBrush.width = 3;
+canvas.renderAll();
+
+function resizeCanvas() {
+  canvas.setHeight(window.innerHeight);
+  canvas.setWidth(window.innerWidth);
   canvas.renderAll();
+}
+/* ***
+ * Initialise canvas size on window load
+ * ***/
 
-  function resizeCanvas() {
-    canvas.setHeight(window.innerHeight);
-    canvas.setWidth(window.innerWidth);
-    canvas.renderAll();
-  }
 
+window.addEventListener('load', function () {
   resizeCanvas();
 });
+/* *************** 
+ * Add undo/redo capability to fabric
+ * ***************/
 
-var undo = function undo() {
-  var lastItemIndex = this.canvas.getObjects().length - 1;
-  var item = canvas.item(lastItemIndex);
-
-  if (item.get('type') === 'path') {
-    this.canvas.remove(item);
-    this.canvas.renderAll();
+var isRedoing = false;
+var h = [];
+canvas.on('object:added', function () {
+  if (!isRedoing) {
+    h = [];
   }
-};
+
+  isRedoing = false;
+});
+
+function undo() {
+  if (canvas._objects.length > 0) {
+    h.push(canvas._objects.pop());
+    canvas.renderAll();
+  }
+}
+
+function redo() {
+  if (h.length > 0) {
+    isRedoing = true;
+    canvas.add(h.pop());
+  }
+}
--- drawing.js
+++ drawing.js
@@ -1,33 +1,50 @@
+
+const canvas = new fabric.Canvas(document.querySelector('canvas'))
+
+canvas.backgroundColor = 'transparent'
+canvas.isDrawingMode = true
+canvas.freeDrawingBrush.color = 'red'
+canvas.freeDrawingBrush.width = 3
+canvas.renderAll();
+
+function resizeCanvas() {
+    canvas.setHeight(window.innerHeight)
+    canvas.setWidth(window.innerWidth)
+    canvas.renderAll()
+}
+
+/* ***
+ * Initialise canvas size on window load
+ * ***/
 window.addEventListener('load', () => {
-    const canvas = new fabric.Canvas('canvas')
-
-    window.addEventListener('resize', resizeCanvas, false)
-
-    canvas.backgroundColor = 'transparent'
-    canvas.isDrawingMode = true
-    canvas.freeDrawingBrush.color = 'red'
-    canvas.freeDrawingBrush.width = 3
-    canvas.renderAll();
-
-    function resizeCanvas() {
-        canvas.setHeight(window.innerHeight)
-        canvas.setWidth(window.innerWidth)
-        canvas.renderAll()
-    }
-
-    
-
     resizeCanvas()
 })
 
-const undo = function() {
-    var lastItemIndex = (this.canvas.getObjects().length - 1);
-    var item = canvas.item(lastItemIndex);
 
-    if(item.get('type') === 'path') {
-        this.canvas.remove(item);
-        this.canvas.renderAll();
+/* *************** 
+ * Add undo/redo capability to fabric
+ * ***************/
+let isRedoing = false;
+let h = [];
+
+canvas.on('object:added', function() {
+    if( !isRedoing ){
+      h = [];
     }
-}
+    isRedoing = false;
+  });
     
+  function undo(){
+    if( canvas._objects.length > 0 ){
+        h.push( canvas._objects.pop() );
+        canvas.renderAll();
+    }
+  }
 
+  function redo(){
+    
+    if( h.length > 0 ){
+        isRedoing = true;
+        canvas.add(h.pop());
+    }
+  }
